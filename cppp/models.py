d5a44076eece1c5f0299957f6f9c39b5b7013f4a
Dominik Straub <dominikstrb@mailbox.org>
2025-12-01 12:01:27 +0000

    Try subjective actor in 2d

--- cppp/models.py
+++ cppp/models.py
@@ -1,6 +1,7 @@
 import jax.numpy as jnp
 from jax.scipy import linalg
 from lqg import Actor, System, Dynamics
+from lqg.tracking.subjective import swap_dims
 
 
 class CorrelatedRelativeObservationBoundedActor(System):
@@ -81,11 +82,81 @@ class CorrelatedObservationBoundedActor(System):
 
         # cost function
         Q = linalg.block_diag(*[jnp.array([[1.0, -1.0], [-1.0, 1.0]])] * dim)
-        R = jnp.diag(action_cost) 
+        R = jnp.diag(action_cost)
 
         spec = Actor(A=A, B=B, F=F, V=V, W=W, Q=Q, R=R, T=T)
 
         super().__init__(actor=spec, dynamics=spec)
+
+
+class CorrelatedObservationSubjectiveActor(System):
+    def __init__(
+        self,
+        sigma_target,
+        sigma_cursor,
+        corr_chol,
+        action_variability,
+        action_cost,
+        dim=2,
+        process_noise=1.0,
+        subj_noise=1.0,
+        subj_vel_noise=0.1,
+        dt=1.0 / 60.0,
+        T=1000,
+    ):
+        self.dim = dim
+        self.process_noise = process_noise
+
+        # dimensionality
+        d = 2 * dim
+
+        # dynamics model
+        A = jnp.eye(d)
+        B = dt * linalg.block_diag(*[jnp.array([[0.0], [1.0]])] * dim)
+
+        # observation model
+        F = jnp.eye(d)
+
+        # noise model
+        V = jnp.diag(
+            jnp.concatenate(jnp.array([(process_noise, a) for a in action_variability]))
+        )
+
+        W_target = jnp.diag(sigma_target) @ corr_chol
+        W_cursor = jnp.diag(sigma_cursor)
+        W = linalg.block_diag(W_target, W_cursor)
+
+        dims = list(range(d)[::2]) + list(range(d)[1::2])
+        W = W[dims, :][:, dims]
+
+        dynamics = Dynamics(A=A, B=B, F=F, V=V, W=W, T=T)
+
+        # subjective dynamics model
+        A = linalg.block_diag(
+            *[jnp.array([[1.0, 0.0, dt], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])] * dim
+        )
+        B = dt * linalg.block_diag(*[jnp.array([[0.0], [1.0], [0.0]])] * dim)
+        F = linalg.block_diag(*[jnp.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])] * dim)
+
+        V = jnp.diag(
+            jnp.concatenate(jnp.array([(subj_noise, a, subj_vel_noise) for a in action_variability]))
+        )
+
+        Q = linalg.block_diag(
+            *[jnp.array([[1.0, -1.0, 0.0], [-1.0, 1.0, 0.0], [0.0, 0.0, 0.0]])] * dim
+        )
+        R = jnp.eye(B.shape[1]) * action_cost
+
+        dims = swap_dims(A.shape[0], dim)
+
+        A = A[dims, :][:, dims]
+        B = B[dims, :]
+        V = V[dims, :]
+        F = F[:, dims]
+        Q = Q[dims, :][:, dims]
+
+        actor = Actor(A=A, B=B, F=F, V=V, W=W, Q=Q, R=R, T=T)
+        super().__init__(actor=actor, dynamics=dynamics)
 
 
 class CorrelatedObservationJerkBoundedActor(System):
@@ -129,8 +200,8 @@ class CorrelatedObservationJerkBoundedActor(System):
                 [0.0, 0.0],
                 [0.0, 0.0],
                 [0.0, 0.0],
-                [0., 0.0],
-                [0.0, 0.],
+                [0.0, 0.0],
+                [0.0, 0.0],
                 [dt, 0.0],
                 [0.0, dt],
             ]
@@ -182,3 +253,35 @@ class CorrelatedObservationJerkBoundedActor(System):
         actor = Actor(A=A, B=B, F=F, V=V, W=W, Q=Q, R=R, T=T)
 
         super().__init__(dynamics=dynamics, actor=actor)
+
+if __name__ == "__main__":
+    from lqg import xcorr
+    from jax import random
+
+    import matplotlib.pyplot as plt
+
+    dt_rw = 1.0 / 60.0
+
+    params = {"process_noise": 1.0, "sigma_target": 6.0, "sigma_cursor": 6.0, "action_variability": 0.5, "action_cost": 0.01}
+
+    model = CorrelatedObservationSubjectiveActor(sigma_target=jnp.array([params["sigma_target"], params["sigma_target"]]),
+                                                sigma_cursor=jnp.array([params["sigma_cursor"], params["sigma_cursor"]]),
+                                                process_noise=params["process_noise"],
+                                                action_variability=jnp.array([params["action_variability"], params["action_variability"]]),
+                                                action_cost=params["action_cost"],
+                                                corr_chol=jnp.array([[1.0, 0.5], [0.0, jnp.sqrt(1 - 0.5 ** 2)]]),
+                                                dt=dt_rw,
+                                                subj_noise=0.1,
+                                                subj_vel_noise=100.,
+                                                T=500)
+
+    x = model.simulate(n=50, rng_key=random.PRNGKey(0))
+
+    vels = jnp.diff(x, axis=-2)
+
+    # plt.plot(x[0, :, 0])  # position
+    # plt.plot(x[0, :, 1])  # response
+    lags, correls = xcorr(vels[..., 1], vels[..., 0], maxlags=100)
+
+    plt.plot(lags[100:] * dt_rw, correls.mean(axis=0)[100:])
+    plt.show()
