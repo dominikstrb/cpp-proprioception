16cf8cd3817a48e26ad067b7754c4ce4104d45af
D. Straub <ds2181@cam.ac.uk>
2026-02-09 17:31:11 +0000

    Add docstring

--- cppp/models/multisensory.py
+++ cppp/models/multisensory.py
@@ -8,14 +8,22 @@ def multisensory_delay_system(A, B, V, Fs, Ws, Q, R, delays=[0, 1], T=500):
 
     d = A.shape[1]
 
+    # get the maximum delay across all sensory modalities to determine how many past states we need to include in the extended state vector
     max_delay = max(delays)
 
+    # stack up the dynamics matrices to work with an extended state vector
+    # the extended state vector contains the current state and the past states up to the maximum delay
+    # this is described in more detail in Izawa & Shadmehr (2008), eqn (3)
     A = linalg.block_diag(A, jnp.diag(jnp.zeros(d * max_delay))) + jnp.diag(
         jnp.ones(d * max_delay), k=-d
     )
 
+    # stack up the control gain matrix to work with the extended state vector
     B = jnp.vstack([B] + [jnp.zeros_like(B)] * max_delay)
 
+    # stack up the sensory feedback matrices to work with the extended state vector
+    # here we apply the appropriate delay to each sensory modality by padding with zeros as needed
+    # this is described in more detail in Crevecoeur et al. (2016)
     F = jnp.vstack(
         [
             jnp.hstack(
@@ -29,11 +37,17 @@ def multisensory_delay_system(A, B, V, Fs, Ws, Q, R, delays=[0, 1], T=500):
         ]
     )
 
+    # stack up the dynamics noise covariance factors to work with the extended state vector
     V = linalg.block_diag(V, jnp.diag(jnp.zeros(d * max_delay)))
+    # the sensory noise covariance factors are block diagonal, with each block corresponding to a different sensory modality
     W = linalg.block_diag(*Ws)
 
+    # stack up the state cost matrix to work with the extended state vector 
+    # the cost is only applied to the current state, not the past states,
+    # which means that the resulting Q is block diagonal with the original Q in the first block and zeros in the remaining blocks
     Q = linalg.block_diag(Q, *[jnp.zeros_like(Q)] * max_delay)
 
+    # create a time-stacked system specification that can be used with the lqg package to solve for the optimal control policy
     spec = time_stack_spec(A=A, B=B, F=F, V=V, W=W, Q=Q, R=R, T=T)
 
     return spec
