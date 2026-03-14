0d57f7b8802e067d9456e951f96f30b26eaa681c
D. Straub <ds2181@cam.ac.uk>
2026-03-14 18:48:36 +0000

    remove legacy file and rename

--- fit_multisensory.py
+++ fit_multisensory.py
@@ -1,5 +1,5 @@
 import argparse
-from jax import random
+from jax import random, numpy as jnp
 import numpyro
 from numpyro.infer import MCMC, NUTS
 from numpyro import distributions as dist
@@ -17,13 +17,13 @@ def parse_args():
         "--participant", type=int, default=135033060, help="Participant ID"
     )
     parser.add_argument(
-        "--vis_delay", type=int, default=2, help="Delay in visual signal (in the model)"
-    )
-    parser.add_argument(
         "--prop_delay",
         type=int,
         default=1,
         help="Delay in proprioceptive signal (in the model)",
+    )
+    parser.add_argument(
+        "--vis_delay", type=int, default=2, help="Delay in visual signal (in the model)"
     )
     parser.add_argument("--seed", type=int, default=1, help="Random seed")
     parser.add_argument(
@@ -33,52 +33,142 @@ def parse_args():
         "--nsamp", type=int, default=2_500, help="Number of samples for NUTS."
     )
     parser.add_argument("--nchain", type=int, default=4, help="Number of chains.")
-    parser.add_argument("--plot", action=argparse.BooleanOptionalAction)
-    parser.add_argument("--save", action=argparse.BooleanOptionalAction, default=True)
+    parser.add_argument(
+        "--model", type=str, default="optimal"
+    )  # TODO: model with separate parameters for multisensory condition, to test whether the same noise parameters can explain both unisensory and multisensory conditions
     return parser.parse_args()
 
 
-def unisensory_model(x, delay=1, dt=0.075):
-    T = x.shape[1]
+def filename_from_args(args):
+    indicator = "_".join(map(str, vars(args).values()))
+    return indicator
+
+
+def optimal_integration_model(data, delays, dt=0.075):
 
     # priors
-    sigma = numpyro.sample("sigma", dist.HalfNormal(10.0))
+    sigma_vis = numpyro.sample("sigma_vis", dist.HalfNormal(10.0).expand([2]))
+    sigma_prop = numpyro.sample("sigma_prop", dist.HalfNormal(10.0))
     action_variability = numpyro.sample("action_variability", dist.HalfNormal(0.5))
     action_cost = numpyro.sample("action_cost", dist.HalfNormal(0.5))
 
-    model = UnisensoryDelayModel(
-        process_noise=1.2,
-        sigma=sigma,
-        action_variability=action_variability,
-        action_cost=action_cost,
-        delay=delay,
-        dt=dt,
-        T=T - 1,
-    )
-    # likelihood
-    numpyro.sample("x", model.conditional_distribution(x), obs=x[:, 1:])
+    for (condition, vis_noise), x in data.items():
+        T = x.shape[1]
+
+        if condition == "multi":
+            model = MultisensoryDelayModel(
+                process_noise=1.2,
+                sigmas=[sigma_prop, sigma_vis[vis_noise - 1]],
+                action_variability=action_variability,
+                action_cost=action_cost,
+                delays=[delays["prop"], delays["vis"]],
+                dt=dt,
+                T=T - 1,
+            )
+        else:
+            delay = delays[condition]
+            model = UnisensoryDelayModel(
+                process_noise=1.2,
+                sigma=(sigma_prop if condition == "prop" else sigma_vis[vis_noise - 1]),
+                action_variability=action_variability,
+                action_cost=action_cost,
+                delay=delay,
+                dt=dt,
+                T=T - 1,
+            )
+
+        # likelihood
+        numpyro.sample(
+            f"x_{condition}_{vis_noise}",
+            model.conditional_distribution(x),
+            obs=x[:, 1:],
+        )
 
 
-def multisensory_model(x, delays, dt=0.075):
-    T = x.shape[1]
-
+def no_integration_model(data, delays, dt=0.075):
     # priors
-    sigmas = numpyro.sample("sigmas", dist.HalfNormal(10.0).expand([2]))
+    sigma_vis = numpyro.sample("sigma_vis", dist.HalfNormal(10.0).expand([2]))
+    sigma_prop = numpyro.sample("sigma_prop", dist.HalfNormal(10.0))
+
     action_variability = numpyro.sample("action_variability", dist.HalfNormal(0.5))
     action_cost = numpyro.sample("action_cost", dist.HalfNormal(0.5))
 
-    model = MultisensoryDelayModel(
-        process_noise=1.2,
-        sigmas=sigmas,
-        action_variability=action_variability,
-        action_cost=action_cost,
-        delays=[delays["prop"], delays["vis"]],
-        dt=dt,
-        T=T - 1,
-    )
-    # likelihood
-    numpyro.sample("x", model.conditional_distribution(x), obs=x[:, 1:])
+    for (condition, vis_noise), x in data.items():
+        T = x.shape[1]
 
+        if condition == "multi":
+            model = UnisensoryDelayModel(
+                process_noise=1.2,
+                sigma=sigma_prop,
+                action_variability=action_variability,
+                action_cost=action_cost,
+                delay=delays["prop"],
+                dt=dt,
+                T=T - 1,
+            )
+        else:
+            delay = delays[condition]
+            model = UnisensoryDelayModel(
+                process_noise=1.2,
+                sigma=(sigma_prop if condition == "prop" else sigma_vis[vis_noise - 1]),
+                action_variability=action_variability,
+                action_cost=action_cost,
+                delay=delay,
+                dt=dt,
+                T=T - 1,
+            )
+
+        # likelihood
+        numpyro.sample(
+            f"x_{condition}_{vis_noise}",
+            model.conditional_distribution(x),
+            obs=x[:, 1:],
+        )
+
+
+def equal_integration_model(data, delays, dt=0.075):
+    # priors
+    sigma_vis = numpyro.sample("sigma_vis", dist.HalfNormal(10.0).expand([2]))
+    sigma_prop = numpyro.sample("sigma_prop", dist.HalfNormal(10.0))
+
+    action_variability = numpyro.sample("action_variability", dist.HalfNormal(0.5))
+    action_cost = numpyro.sample("action_cost", dist.HalfNormal(0.5))
+
+    for (condition, vis_noise), x in data.items():
+        T = x.shape[1]
+
+        if condition == "multi":
+            sigma = jnp.sqrt((sigma_vis[vis_noise - 1] ** 2 + sigma_prop**2) / 2)
+            model = MultisensoryDelayModel(
+                process_noise=1.2,
+                sigmas=[sigma, sigma],
+                action_variability=action_variability,
+                action_cost=action_cost,
+                delays=[delays["prop"], delays["vis"]],
+                dt=dt,
+                T=T - 1,
+            )
+        else:
+            delay = delays[condition]
+            model = UnisensoryDelayModel(
+                process_noise=1.2,
+                sigma=(sigma_prop if condition == "prop" else sigma_vis[vis_noise - 1]),
+                action_variability=action_variability,
+                action_cost=action_cost,
+                delay=delay,
+                dt=dt,
+                T=T - 1,
+            )
+
+        # likelihood
+        numpyro.sample(
+            f"x_{condition}_{vis_noise}",
+            model.conditional_distribution(x),
+            obs=x[:, 1:],
+        )
+
+
+models = {"optimal": optimal_integration_model, "no_integration": no_integration_model, "equal_integration": equal_integration_model}
 
 if __name__ == "__main__":
     args = parse_args()
@@ -88,68 +178,29 @@ if __name__ == "__main__":
 
     delays = {"vis": args.vis_delay, "prop": args.prop_delay}
 
-    # for both levels of visual noise in the experiment
-    for vis_noise in [1, 2]:
-
-        # load data from multisensory condition
-        data = preprocess_multisensory_data(
-            df,
-            participant=args.participant,
-            condition="multi",
-            vis_noise=vis_noise,
-        )
-
-        # fit multisensory model
-        nuts_kernel = NUTS(multisensory_model)
-        mcmc_xy = MCMC(
-            nuts_kernel,
-            num_warmup=args.nwarmup,
-            num_samples=args.nsamp,
-            num_chains=4,
-        )
-        mcmc_xy.run(
-            random.PRNGKey(args.seed),
-            data,
-            delays=delays,
-            dt=0.075,
-        )
-
-        # save model fit
-        inference_data = az.from_numpyro(mcmc_xy)
-        if args.save:
-            inference_data.to_netcdf(
-                f"results/multisensory-mcmc-{args.participant}-multi-{vis_noise}-{args.seed}.nc"
-            )
-
-        # fit unisensory conditions
-        for condition in ["vis", "prop"]:
-            # load unisensory data
-            data = preprocess_multisensory_data(
+    # load data for all conditions and visual noise levels
+    data = {}
+    for condition in df["type"].unique():
+        for vis_noise in df["vis_noise"].unique():
+            data[(condition, vis_noise)] = preprocess_multisensory_data(
                 df,
                 participant=args.participant,
                 condition=condition,
                 vis_noise=vis_noise,
             )
 
-            # fit model
-            nuts_kernel = NUTS(unisensory_model)
-            mcmc_xy = MCMC(
-                nuts_kernel,
-                num_warmup=args.nwarmup,
-                num_samples=args.nsamp,
-                num_chains=4,
-            )
-            mcmc_xy.run(
-                random.PRNGKey(args.seed),
-                data,
-                delay=delays[condition],
-                dt=0.075,
-            )
+    # fit joint model
+    nuts_kernel = NUTS(models[args.model])
+    mcmc_xy = MCMC(
+        nuts_kernel, num_warmup=args.nwarmup, num_samples=args.nsamp, num_chains=4
+    )
+    mcmc_xy.run(
+        random.PRNGKey(args.seed),
+        data,
+        delays=delays,
+        dt=0.075,
+    )
 
-            # save model fit
-            inference_data = az.from_numpyro(mcmc_xy)
-
-            if args.save:
-                inference_data.to_netcdf(
-                    f"results/multisensory-mcmc-{args.participant}-{condition}-{vis_noise}-{args.seed}.nc"
-                )
+    # save model fit
+    inference_data = az.from_numpyro(mcmc_xy)
+    inference_data.to_netcdf(f"results/multisensory-mcmc-{filename_from_args(args)}.nc")
